86846f7ff729932c23188436709bda3b8e6122bd
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2025-02-18 00:20:50 +0500

    setup o auth 2 lcient

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import {google} from "googleapis";
 dotenv.config();
+
+const oAuth2client = new google.auth.OAuth2({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI
+})
+
+oAuth2client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+});
+const tasks = google.tasks({
+    version: 'v1',
+    auth: oAuth2client
+});
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
+const main = async () => {
 
-const PORT = process.env.PORT || 5000;
+    const res = await tasks.tasklists.list();
+      console.log(res.data);
+}
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+main().catch(console.error);
