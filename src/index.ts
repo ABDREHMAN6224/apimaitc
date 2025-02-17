484d0b59e0423aba420a39c19af260fe3d17c727
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2025-02-18 00:25:59 +0500

    fetch tasks from api

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {google} from "googleapis";
+import path from 'path';
 dotenv.config();
 
-const oAuth2client = new google.auth.OAuth2({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: process.env.REDIRECT_URI
-})
-
-oAuth2client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN
-});
+const auth = new google.auth.GoogleAuth({
+    keyFile: path.join(__dirname, 'keys.json'),
+    scopes: ['https://www.googleapis.com/auth/tasks'],
+  });
 const tasks = google.tasks({
     version: 'v1',
-    auth: oAuth2client
+    auth: auth,
 });
-
-const app = express();
 
 
 const main = async () => {
