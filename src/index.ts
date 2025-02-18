1a764d7b79efcc253bbecacb0d5213bb683166cf
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2025-02-18 09:30:25 +0500

    used local auth

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,27 @@ import dotenv from 'dotenv';
 import {google} from "googleapis";
 import path from 'path';
 import { Client } from '@notionhq/client';
+// @ts-ignore
+import { authenticate } from "@google-cloud/local-auth";
 dotenv.config();
 
-const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname, 'keys.json'),
-    scopes: ['https://www.googleapis.com/auth/tasks'],
-  });
-const tasks = google.tasks({
-    version: 'v1',
-    auth: auth,
-});
+// const auth = new google.auth.GoogleAuth({
+//     keyFile: path.join(__dirname, 'keys.json'),
+//     scopes: ['https://www.googleapis.com/auth/tasks'],
+//   });
 
+const SCOPES = ["https://www.googleapis.com/auth/tasks"];
 
-const main = async () => {
-
-    const res = await tasks.tasklists.list();
-      console.log(res.data);
+async function authorize() {
+    return await authenticate({
+        keyfilePath: path.join(__dirname, "credentials.json"), 
+        scopes: SCOPES,
+    });
 }
 
-// main().catch(console.error);
+
+
+
 
 const notion = new Client({ auth: process.env.NOTION_SECRET});
 const databaseId = process.env.NOTION_DB_ID;
@@ -45,5 +47,22 @@ async function addTask(title:string, dueDate:string) {
     }
 }
 
-// Example usage
-addTask("Complete Notion API Integration", "2025-02-20");
+const main = async () => {
+    const auth = await authorize();
+    const tasks = google.tasks({version: 'v1', auth});
+    const res = await tasks.tasklists.list();
+    const listData = res.data.items?.map((item) => {return {id: item.id, title: item.title,updated: item.updated}});
+    listData?.forEach(async (list) => {
+        const res = await tasks.tasks.list({
+            tasklist: list.id!,
+            showHidden: true,
+            showAssigned: true,
+            showDeleted: true,
+            showCompleted: true,
+        })
+        console.log(res.data.items?.map((item) => {return {id: item.id, title: item.title,updated: item.updated,due: item.due,completed: item.completed??false}}));
+        // const tasksData = res.data.items?.map((item) => {return {id: item.id, title: item.title,updated: item.updated}});
+    });
+}
+
+main().catch(console.error);
